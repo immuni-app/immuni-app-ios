72c58ca071b97aaf9eb99657c2f099693e255d89
Lorenzo D'Eri <ld@bendingspoons.com>
2020-10-30 13:18:43 +0100

    test: remove unnecessary imports

--- AppTests/Extensions/DateExtensionTests.swift
+++ AppTests/Extensions/DateExtensionTests.swift
@@ -12,10 +12,6 @@
 // You should have received a copy of the GNU Affero General Public License
 // along with this program. If not, see <https://www.gnu.org/licenses/>.
 
-import CryptoKit
-import Foundation
-import Hydra
-import Persistence
 import XCTest
 
 final class DateExtensionsTests: XCTestCase {
